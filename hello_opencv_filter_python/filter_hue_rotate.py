959164b49a952b78ac3d638651b3feebc2e37c80
Yusuke Kawamoto <novogrammer@gmail.com>
2023-10-07 15:11:45 +0900

    コメント追記

--- hello_opencv_filter_python/filter_hue_rotate.py
+++ hello_opencv_filter_python/filter_hue_rotate.py
@@ -11,6 +11,7 @@ def filter_hue_rotate(img_before:cv2.UMat)->cv2.UMat:
   tshift=int((t/10.0*256)%256)
   img_hsv=cv2.cvtColor(img_before,cv2.COLOR_BGR2HSV_FULL)
   img_hsv_rotated:cv2.UMat=img_hsv.copy()
+  # 暗黙の剰余計算を行う
   img_hsv_rotated[:,:,0]=img_hsv[:,:,0]+tshift
 
   img_after=cv2.cvtColor(img_hsv_rotated,cv2.COLOR_HSV2BGR_FULL)
