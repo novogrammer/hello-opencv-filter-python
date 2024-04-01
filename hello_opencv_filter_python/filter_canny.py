06baff94375fb5a0bcf419d0d89444c70aa16844
Yusuke Kawamoto <novogrammer@gmail.com>
2024-04-01 14:05:52 +0900

    画像合成する都合上、cannyの結果をBGRに変更しておく。

--- hello_opencv_filter_python/filter_canny.py
+++ hello_opencv_filter_python/filter_canny.py
@@ -6,7 +6,8 @@ from runner import run
 
 def filter_canny(img_before:cv2.UMat)->cv2.UMat:
   img_gray = cv2.cvtColor(img_before, cv2.COLOR_BGR2GRAY)
-  img_after = cv2.Canny(img_gray, 100, 200)
+  canny = cv2.Canny(img_gray, 100, 200)
+  img_after = cv2.cvtColor(canny, cv2.COLOR_GRAY2BGR)
   return img_after
 
 if __name__ == '__main__':
