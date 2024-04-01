87a5f981c46123ce35b19d34327c0d54072aba3d
Yusuke Kawamoto <novogrammer@gmail.com>
2024-04-01 17:53:32 +0900

    grayのmedianを取得する

--- hello_opencv_filter_python/filter_canny.py
+++ hello_opencv_filter_python/filter_canny.py
@@ -6,11 +6,11 @@ from runner import run
 
 
 def filter_canny(img_before:cv2.UMat)->cv2.UMat:
+  img_gray = cv2.cvtColor(img_before, cv2.COLOR_BGR2GRAY)
   sigma = 0.33
-  med = np.median(img_before)
+  med = np.median(img_gray)
   th1 = int(max(0, (1-sigma)*med))
   th2 = int(min(255, (1+sigma)*med))  
-  img_gray = cv2.cvtColor(img_before, cv2.COLOR_BGR2GRAY)
   canny = cv2.Canny(img_gray, th1, th2)
   img_after = cv2.cvtColor(canny, cv2.COLOR_GRAY2BGR)
   return img_after
